afa8be30a6bbf9ba74b8d8809d0a44a4858a85db
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2014-07-03 09:37:20 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Backup remote mikrotik config
+=============================
+
+Script made and download config backup to server in text and binary formats.
+
+To use this script you need:
+
+1. Register user on mikrotik with login and password used in script.
+2. Upload public ssh key and setup it to this user.
---
+++ backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+routers=( 192.168.1.1 )
+backupdir="/home/backup/mikrotik"
+privatekey="/root/.ssh/id_dsa"
+login="user"
+passwd="pa$Sw0rd"
+fulldir="${backupdir}/`date +%Y`/`date +%m`/`date +%d`"
+
+for r in ${routers[@]}; do
+    cmd_backup="/system backup save name=${r}.backup"
+    ssh ${login}@$r -i $privatekey "${cmd_backup}" > /dev/null
+    cmd_backup="/export file=${r}"
+    ssh ${login}@$r -i $privatekey "${cmd_backup}" > /dev/null
+    sleep 5
+    mkdir -p $fulldir
+    wget -qP $fulldir ftp://${login}:${passwd}@${r}/${r}.backup
+    wget -qP $fulldir ftp://${login}:${passwd}@${r}/${r}.rsc
+    ssh ${login}@$r -i $privatekey "/file remove \"${r}.backup\""
+    ssh ${login}@$r -i $privatekey "/file remove \"${r}.rsc\""
+done
